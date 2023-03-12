13e1501a4ac6cc0474edfed5c97fb64e3f68c274
Sujal <Sujal@DESKTOP-JLKCJIL>
2023-03-13 00:50:26 +1300

    Add project files.

---
+++ IslandFoodmart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IslandFoodmart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ IslandFoodmart/Views/Home/ContactUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace IslandFoodmart.Views.Home
+{
+    public class ContactUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
