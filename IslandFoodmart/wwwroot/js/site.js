2d42d893a87a1bc113ed939623d76e68fc387fb8
Sujal Chand <sujalchand2526@gmail.com>
2023-03-13 21:44:21 +1300

    Added a image carousel that changes images with button clicks and also automatically every 4 seconds.

--- IslandFoodmart/wwwroot/js/site.js
+++ IslandFoodmart/wwwroot/js/site.js
@@ -2,3 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+const carousel = document.querySelector('.carousel');
+const container = document.querySelector('.carousel-container');
+const prevBtn = document.querySelector('.carousel-prev');
+const nextBtn = document.querySelector('.carousel-next');
+const images = document.querySelectorAll('.carousel img');
+
+let currentIndex = 0;
+const maxIndex = images.length - 1;
+const imageWidth = images[0].clientWidth;
+
+function moveTo(index) {
+    currentIndex = index;
+    container.style.transform = `translateX(-${currentIndex * imageWidth}px)`;
+}
+
+prevBtn.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        moveTo(currentIndex - 1);
+    } else {
+        moveTo(maxIndex);
+    }
+});
+
+nextBtn.addEventListener('click', () => {
+    if (currentIndex < maxIndex) {
+        moveTo(currentIndex + 1);
+    } else {
+        moveTo(0);
+    }
+});
+
+moveTo(currentIndex);
+
+setInterval(() => {
+    if (currentIndex < maxIndex) {
+        moveTo(currentIndex + 1);
+    } else {
+        moveTo(0);
+    }
+}, 4000);
