2db150f3e21b26a697b617d7a859279d6610838f
Sujal Chand <ac111360@avcol.school.nz>
2023-05-31 20:49:51 +1200

    Modfied the contact us page

--- IslandFoodmart/Areas/Identity/Data/User.cs
+++ IslandFoodmart/Areas/Identity/Data/User.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+
 namespace IslandFoodmart.Areas.Identity.Data
 {
     public class User : IdentityUser
