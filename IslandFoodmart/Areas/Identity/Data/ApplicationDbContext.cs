2fa7a35607f6bfb4bf3f8ff46cfe54615a67eeba
sujalchand <ac111360@avcol.school.nz>
2023-08-09 13:34:31 +1200

    Seeding Products

--- IslandFoodmart/Areas/Identity/Data/ApplicationDbContext.cs
+++ IslandFoodmart/Areas/Identity/Data/ApplicationDbContext.cs
@@ -27,6 +27,27 @@ namespace IslandFoodmart.Areas.Identity.Data
                 new Category { CategoryID = 3, CategoryName = "Drinks" },
                 new Category { CategoryID = 4, CategoryName = "Pantry" },
                 new Category { CategoryID = 5, CategoryName = "Personal Care" });
+           /* builder.Entity<Product>().HasData(
+                new Product { ProductID = 1, CategoryID = 3, ImagePath = "image", ProductName = "V Drink 250ml", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 2, CategoryID = 3, ImagePath = "image", ProductName = "Coke 440ml", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 3, CategoryID = 3, ImagePath = "image", ProductName = "Sprite", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 4, CategoryID = 3, ImagePath = "image", ProductName = "Red Bull", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 5, CategoryID = 3, ImagePath = "image", ProductName = "Lift", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 6, CategoryID = 3, ImagePath = "image", ProductName = "Big Ben Mince & Cheese ", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 7, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 8, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 9, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 10, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 11, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 12, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 13, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 14, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 15, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 16, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 17, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 18, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 19, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 },
+                new Product { ProductID = 20, CategoryID = 3, ImagePath = "image", ProductName = "V Drink", ProductPrice = 4, ProductStock = 20 }); */
             base.OnModelCreating(builder);
             builder.ApplyConfiguration(new ApplicationUserEntityConfiguration());
         }
